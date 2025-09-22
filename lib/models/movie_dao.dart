94d294a1381407d0398ade22b6d963727f2083ec
Ali-Vladimir <alivladimirc@gmail.com>
2025-09-22 15:48:50 -0600

    feat: Add MovieDao model and implement SELECT method in MoviesDatabase

---
+++ lib/models/movie_dao.dart
@@ -0,0 +1,17 @@
+class MovieDao {
+  int? idMovie;
+  String? nameMovie;
+  String? timeMovie;
+  String? dateRelease;
+
+  MovieDao({this.idMovie, this.nameMovie, this.timeMovie, this.dateRelease});
+
+  factory MovieDao.fromMap(Map<String,dynamic> mapa) {
+    return MovieDao(
+      idMovie: mapa['idMovie'],
+      nameMovie: mapa['nameMovie'],
+      timeMovie: mapa['timeMovie'],
+      dateRelease: mapa['dateRelease']
+    );
+  }
+}
